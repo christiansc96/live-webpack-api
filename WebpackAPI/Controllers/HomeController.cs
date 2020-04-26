5729d7c7a956de0d43bacf1e77e90a17af320044
Christian Sánchez <cdsanchezc96@gmail.com>
2020-04-26 10:29:11 -0600

    Add post email

--- WebpackAPI/Controllers/HomeController.cs
+++ WebpackAPI/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace WebpackAPI.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
             return View();
         }
     }
--- WebpackAPI/Controllers/TalleresController.cs
+++ WebpackAPI/Controllers/TalleresController.cs
@@ -6,10 +6,12 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using WebpackAPI.DB;
+using WebpackAPI.Models;
 
 namespace WebpackAPI.Controllers
 {
@@ -24,10 +26,60 @@ namespace WebpackAPI.Controllers
             return await Task.FromResult(Ok(talleres));
         }
 
-        // GET: api/Talleres
-        //public IHttpActionResult GetTalleres()
+        //[HttpGet("{id}")]
+        //public async Task<IHttpActionResult> GetTaller(int id)
         //{
-        //    return Ok(db.Talleres);
+        //    var talleres = await context.Talleres.FindAsync(id);
+        //    if (talleres == null)
+        //    {
+        //        return await Task.FromResult(NotFound());
+        //    }
+        //    return await Task.FromResult(Ok(talleres));
         //}
+
+        [HttpPost]
+        public async Task<IHttpActionResult> PostEmail(EmailDTO email)
+        {
+            if (email == null)
+            {
+                return await Task.FromResult(BadRequest());
+            }
+
+            var bodyEmail = "<p>Hola " + email.Name + "! <br/><br/> DevTeam504 WebApp te informa que hemos " +
+            "recibido tu mensaje. <br/><br/> Cuerpo del Mensaje: " + email.Message + " <br/> Fecha del Mensaje: "
+            + DateTime.Now + " <br/><br/> Su mensaje fue remitido a las personas encargadas. <br/> Pronto" +
+            " nos pondremos en contacto con usted. <br/>Saludos!</p>";
+
+            MailMessage correo = new MailMessage
+            {
+                Subject = "Nuevo Mensaje DevTeam504",
+                Body = bodyEmail,
+                IsBodyHtml = true,
+                BodyEncoding = System.Text.Encoding.UTF8,
+                SubjectEncoding = System.Text.Encoding.Default,
+                From = new MailAddress("catrachosdev@gmail.com")
+            };
+
+            correo.To.Add(new MailAddress(email.Email ?? "christian.sanchez@cit.hn"));
+            SmtpClient mail = new SmtpClient
+            {
+                Port = 587,
+                Host = "smtp.gmail.com",
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new System.Net.NetworkCredential("catrachosdev@gmail.com", "Honduras.123")
+            };
+
+            try
+            {
+                await mail.SendMailAsync(correo);
+            }
+            catch
+            {
+                return await Task.FromResult(BadRequest());
+            }
+            return await Task.FromResult(Ok());
+        }
     }
 }
