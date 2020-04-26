072b5aa37a3a58c4405c0c4bc1b632d3915186c1
Christian Sánchez <cdsanchezc96@gmail.com>
2020-04-26 07:59:50 -0600

    first commit

---
+++ WebpackAPI/Controllers/TalleresController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebpackAPI.DB;
+
+namespace WebpackAPI.Controllers
+{
+    public class TalleresController : ApiController
+    {
+        private DevTeam504Entities context = new DevTeam504Entities();
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetTalleres()
+        {
+            var talleres = await context.Talleres.ToListAsync();
+            return await Task.FromResult(Ok(talleres));
+        }
+
+        // GET: api/Talleres
+        //public IHttpActionResult GetTalleres()
+        //{
+        //    return Ok(db.Talleres);
+        //}
+    }
+}
---
+++ WebpackAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebpackAPI.WebApiApplication" Language="C#" %>
